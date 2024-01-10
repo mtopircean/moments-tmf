d6407f71ec86e64a29a611e3316591602e747d6f
Marius Topircean <mtopircean@yahoo.com>
2024-01-10 21:22:53 +0000

    Intermediary commit with forms and context

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://drf-720f24919634.herokuapp.com/' /*link form to your app url */
+axios.defaults.headers.post['Content-Type'] = 'mutipart/form-data' /*Data handled by form*/
+axios.defaults.withCredentials = true /*to handle cookies*/
