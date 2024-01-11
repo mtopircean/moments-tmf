f2915895155e650dcde1609ca6987b50506d7b57
Marius Topircean <mtopircean@yahoo.com>
2024-01-11 18:35:19 +0000

    Created navbar links, user views, burger menu retraction

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://drf-720f24919634.herokuapp.com/' /*link form to your app url */
 axios.defaults.headers.post['Content-Type'] = 'mutipart/form-data' /*Data handled by form*/
 axios.defaults.withCredentials = true /*to handle cookies*/
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
